132b43e151879d230536969d022a4382abb30448
qinzzz <wangqinxin2007@126.com>
2021-11-22 15:44:55 -0500

    profiler v1.1: add whole data profiling

--- examples/profiler/data_processing_profiler.py
+++ examples/profiler/data_processing_profiler.py
@@ -36,7 +36,7 @@ class DummyPackProcessor(PackProcessor):
 
 class OntonoteDataProcessing():
     def __init__(self, file_path = "data_samples/profiler/combine_data"):
-        root_path = os.path.abspath(
+        self.root_path = os.path.abspath(
             os.path.join(
                 os.path.dirname(os.path.abspath(__file__)),
                 os.pardir,
@@ -44,7 +44,7 @@ class OntonoteDataProcessing():
             )
         )
         self.dataset_path = os.path.join(
-            root_path, file_path
+            self.root_path, file_path
         )
         # Define and config the Pipeline
         self.nlp = Pipeline[DataPack]()
@@ -63,29 +63,42 @@ class OntonoteDataProcessing():
         self.nlp.initialize()
 
         self.tokenizer = tx.data.BERTTokenizer(pretrained_model_name="bert-base-uncased")
-        
-
-    def data_processing(self):
+    
+    def whole_data_processing(self):
+        file_path = "data_samples/profiler/whole_data"
+        dataset_path = os.path.join(
+            self.root_path, file_path
+        )
         tokenize_time = 0
         get_document_time = 0
         get_sentence_time = 0
         get_token_entity_time = 0
         get_data_time = 0
         iter_time = 0
+        get_token_entity_from_document_time = 0
         # get processed pack from dataset
         bg = time.time()
-        iter = self.nlp.process_dataset(self.dataset_path)
+        iter = self.nlp.process_dataset(dataset_path)
         # print("length of iterator: ", len(iter))
         process_time = time.time() - bg
 
+        """
+        iter_time:
+        Iterate through the dataset and get each datapack
+        """
         it = time.time()
-        for i, pack in enumerate(iter):
-            """
-            pack: DataTuple object
-            """
+        packcnt = 0
+        sentcnt = 0
+        token_sent_cnt = 0
+        token_doc_cnt = 0
+        for pack in iter:
+            packcnt += 1
             iter_time += time.time() - it
-            # print(f"DataTuple {i}, has {len(pack.elements)} elements")
-            # get sentence from pack
+            
+            """
+                get_document_time:
+                Get document from databack (expected to have only one document each pack)
+            """
             dt = time.time()
             document = []
             for doc in pack.get(Document):
@@ -97,40 +110,55 @@ class OntonoteDataProcessing():
             get_document_time += time.time() - dt
 
             """
-                test get_raw and get_data_raw: 
-                    get text of all sentences
-                    get tokens from each sentence
-                    compare the results of get_raw and get_data_raw
+                get_sentence_time:
+                Get sentence from databack
             """
             st = time.time()
             sentences = []
             for sent in pack.get(Sentence):
-                
+                sentcnt += 1
                 sent_text = sent.text
                 sentences.append(sent_text)
+                get_sentence_time += time.time()-st
                 
                 tet = time.time()
                 tokenized_tokens = []
-                tokens = [pack.get(Token, sent)]
+                tokens = [t.text for t in pack.get(Token, sent)]
                 entities = [entity.text
                     for entity in pack.get(EntityMention, sent)
                 ]
                 get_token_entity_time += time.time() - tet
 
+                """
+                tokenize_time: 
+                Tokenize tokens and entities with BERTTokenizer
+                """
                 tt = time.time()
                 tokenized_tokens += self.tokenizer.map_text_to_id(sent_text)
                 token_ids = self.tokenizer.map_token_to_id(tokens)
                 entity_ids = self.tokenizer.map_token_to_id(entities)
                 tokenize_time += time.time()-tt
 
-                # print(colored("map_text_to_id:", "red"), len(tokenized_tokens), "\n")
-                # print(colored("map_token_to_id:", "yellow"), len(token_ids), "\n")
-                # print(colored("map_entity_to_id:", "yellow"), len(entity_ids), "\n")
+                st = time.time()
             
-            sentences_text = " ".join(sentences)
-            # assert(sentences_text == document_text)
-            get_sentence_time += time.time()-st
+            """
+            get_token_entity_from_document_time:
+            Get token and entity from the whole document
+            """
+            tokdocTime = time.time()
+            tokenized_tokens = []
+            tokens = [t.text for t in pack.get(Token, document)]
+            token_doc_cnt += len(tokens)
+            entities = [entity.text
+                for entity in pack.get(EntityMention, document)
+            ]
+            get_token_entity_from_document_time += time.time() - tokdocTime
 
+            
+            """
+            get_data_time:
+            Get sentence attributes with get_data request
+            """
             datat = time.time()
             sentences = []
             request = {
@@ -149,16 +177,139 @@ class OntonoteDataProcessing():
             "tokenize_time": tokenize_time,
             "get_document_time": get_document_time,
             "get_sentence_time": get_sentence_time,
-            "get_token_entity_time": get_token_entity_time,
+            "get_token_entity_from_sentence_time": get_token_entity_time,
+            "get_token_entity_from_document_time": get_token_entity_from_document_time,
             "get_data_time": get_data_time,
             "iter_time": iter_time
         }
+        print("datapack count:", packcnt)
+        print("sentence count:", sentcnt)
+        print("tokens count", token_sent_cnt, token_doc_cnt)
+        return time_dict
+
+    def data_processing(self):
+        tokenize_time = 0
+        get_document_time = 0
+        get_sentence_time = 0
+        get_token_entity_time = 0
+        get_data_time = 0
+        iter_time = 0
+        get_token_entity_from_document_time = 0
+        # get processed pack from dataset
+        bg = time.time()
+        iter = self.nlp.process_dataset(self.dataset_path)
+        # print("length of iterator: ", len(iter))
+        process_time = time.time() - bg
+
+        """
+        iter_time:
+        Iterate through the dataset and get each datapack
+        """
+        it = time.time()
+        packcnt = 0
+        sentcnt = 0
+        token_sent_cnt = 0
+        token_doc_cnt = 0
+        for pack in iter:
+            packcnt += 1
+            iter_time += time.time() - it
+            
+            """
+                get_document_time:
+                Get document from databack (expected to have only one document each pack)
+            """
+            dt = time.time()
+            document = []
+            for doc in pack.get(Document):
+                document.append(doc)
+            if len(document) > 1:
+                raise RuntimeError("More than one document in a datatuple")
+            document = document[0]
+            document_text = document.text
+            get_document_time += time.time() - dt
+
+            """
+                get_sentence_time:
+                Get sentence from databack
+            """
+            st = time.time()
+            sentences = []
+            for sent in pack.get(Sentence):
+                sentcnt += 1
+                sent_text = sent.text
+                sentences.append(sent_text)
+                get_sentence_time += time.time()-st
+                
+                tet = time.time()
+                tokenized_tokens = []
+                tokens = [t.text for t in pack.get(Token, sent)]
+                entities = [entity.text
+                    for entity in pack.get(EntityMention, sent)
+                ]
+                get_token_entity_time += time.time() - tet
+
+                """
+                tokenize_time: 
+                Tokenize tokens and entities with BERTTokenizer
+                """
+                tt = time.time()
+                tokenized_tokens += self.tokenizer.map_text_to_id(sent_text)
+                token_ids = self.tokenizer.map_token_to_id(tokens)
+                entity_ids = self.tokenizer.map_token_to_id(entities)
+                tokenize_time += time.time()-tt
+
+                st = time.time()
+            
+            """
+            get_token_entity_from_document_time:
+            Get token and entity from the whole document
+            """
+            tokdocTime = time.time()
+            tokenized_tokens = []
+            tokens = [t.text for t in pack.get(Token, document)]
+            token_doc_cnt += len(tokens)
+            entities = [entity.text
+                for entity in pack.get(EntityMention, document)
+            ]
+            get_token_entity_from_document_time += time.time() - tokdocTime
+
+            
+            """
+            get_data_time:
+            Get sentence attributes with get_data request
+            """
+            datat = time.time()
+            sentences = []
+            request = {
+                Sentence: ["speaker", "part_id"]
+            }
+            for sent in pack.get_data(Sentence, request):
+                sentences.append(sent['context'])
+            
+            sentences_text_comp = " ".join(sentences)
+            # assert(sentences_text == sentences_text_comp)
+            get_data_time += time.time() - datat
+            it = time.time()
+        
+        time_dict = {
+            "process_time": process_time,
+            "tokenize_time": tokenize_time,
+            "get_document_time": get_document_time,
+            "get_sentence_time": get_sentence_time,
+            "get_token_entity_from_sentence_time": get_token_entity_time,
+            "get_token_entity_from_document_time": get_token_entity_from_document_time,
+            "get_data_time": get_data_time,
+            "iter_time": iter_time
+        }
+        print("datapack count:", packcnt)
+        print("sentence count:", sentcnt)
+        print("tokens count", token_sent_cnt, token_doc_cnt)
         return time_dict
 
 if __name__ == "__main__":
     data_pipeline = OntonoteDataProcessing()
     t1 = time.time()
-    time_dict = data_pipeline.data_processing()
+    time_dict = data_pipeline.whole_data_processing()
     t2 = time.time()
     print("total time spent: ", t2-t1)
     print(f"profiling time: {time_dict}")
